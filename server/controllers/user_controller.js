3378b859bfac157689d2d794a41b377af9eb7889
Soratica <timlam3535@gmail.com>
2023-07-12 17:19:06 -0400

    user authentication and sign in

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,5 +1,10 @@
 const User = require('../models/user_model')
+const jwt = require('jsonwebtoken')
 
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // Sign Up User
 const SignUpUser = async (req, res) => {
@@ -8,7 +13,12 @@ const SignUpUser = async (req, res) => {
   try {
     const user = await User.signup(email, password)
 
-    res.status(200).json({email, user})
+    //create a token
+    const token = createToken(user._id)
+
+    console.log(token)
+
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
